5851cb5d57ff2e2c09dda5405e86d122f6dc3623
cubercubegit <102414081+cubercubegit@users.noreply.github.com>
2025-08-03 03:32:48 -0500

    Add player model, example players, and leaderboard page with links

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,5 @@
+class PlayersController < ApplicationController
+  def index
+    @players = Player.order(ranking: :desc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "pages#index"
 
+  get "/players", to: "players#index"
+  get "/pages", to: "pages#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
